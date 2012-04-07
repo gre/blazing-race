bd82e49987054fecc5c3344301d75d6364a248c9
Gaetan Renaudeau <renaudeau.gaetan@gmail.com>
2012-04-08 00:52:55 +0200

    continued converter

--- maps/converter/blazingRaceConverter.js
+++ maps/converter/blazingRaceConverter.js
@@ -1,132 +1,135 @@
-var $ = require("jquery");
-var jsdom = require("jsdom");
-var fs = require('fs');
-
-/*
-self = window = jsdom.createWindow();
-THREE = require("./lib/Three.js").THREE;
-//console.log(THREE);
-require("./lib/ThreeExtras.js");
-//console.log(THREE);
-*/
-
-var path = require("path");
-var lib = path.join(path.dirname(fs.realpathSync(__filename)), './lib');
+(function(){
 
 
-
-/*
-function parsePosition (text) {
-  var p = text.trim().split(/\ +/);
-  return {
-    x: parseFloat(p[0]),
-    y: parseFloat(p[1])
-  };
-}
-
-function parsePositions (text) {
-  var raw = text.trim().split(/\ +/);
-  var arr = [];
-  for (var i = 0; i < raw.length; i += 3) {
-    arr.push({
-      x: parseFloat(raw[i]),
-      y: parseFloat(raw[i+1])
-    });
-  }
-  return arr;
-}
-
-function parseIntArray (text) {
-  var raw = text.split(/\ +/);
-  var arr = [];
-  for (var i = 0; i < raw.length; ++i) {
-    if (raw[i]!=='') 
-      arr.push(parseInt(raw[i]));
-  }
-  return arr;
-}
-
-function getPolygons ($positions, $polylist) {
-  var positions = parsePositions($positions.text());
-  var faces = parseIntArray($polylist.find("p").text());
-  var vcount = parseIntArray($polylist.find("vcount").text());
-  var inputcount = $polylist.find("input");
-  var maxoffset = inputcount.map(function(i,node){
-    return $(node).attr("offset") || 0;
-  }).sort()[inputcount.size()-1] || 0;
-
-  var tris=[];
-  var cnt=0;
-  for(n=0; n<vcount.length; n++) {
-    for(var j=0; j<vcount[n]-2; j++) {
-      for(var k=0; k<=maxoffset; k++){
-        tris.push(faces[cnt+k]);
-      }
-      for(k=0; k<=maxoffset; k++){
-        tris.push(faces[cnt+(maxoffset+1)*(j+1)+k]);
-      }
-      for(k=0; k<=maxoffset; k++){
-        tris.push(faces[cnt+(maxoffset+1)*(j+2)+k]);
-      }
-    }
-    cnt = cnt+(maxoffset+1)*vcount[n];
-  }
-  console.log(tris);
-
-}
-*/
+var Z_GROUND = 0
+  , Z_CANDLE = 1
+  , Z_START = 2
+  , Z_WATER = -1
+  , Z_NO_OXYGEN = -2
+  , Z_WORLD_SCALE = -10
+  ;
 
 function Converter (collada) {
-  var doc = $(collada);
 
-  var cb;
-  var result;
+  this.convert = function () {
+    var loader = new THREE.ColladaLoader();
+    var xml = (new DOMParser()).parseFromString(collada, "text/xml");
 
-  jsdom.env({
-    html: "<html><head></head><body></body></html>",
-    src: [
-    fs.readFileSync(lib+"/Three.js").toString(),
-    fs.readFileSync(lib+"/ThreeExtras.js").toString()
-    ],
-    done: function(errors, window) {
-      var THREE = window.THREE;
-      var loader = new THREE.ColladaLoader();
-      var obj = loader.parse(collada);
-      console.log(obj);
-      result = { todo : true };
-      cb && cb(result);
-    }
-  });
-  /*
-  var objects = doc.find("library_visual_scenes node").map(function (i, n) {
-    var node = $(n);
-    var ret = {
-      id: node.attr("id"),
-      position: parsePosition(node.find("translate:first").text())
+    var c = loader.parse(xml);
+    C = c;
+
+    var map = {
+      objects: {
+        candles: [],
+        grounds: [],
+        waters:  [],
+        noOxygens: []
+      }
     };
-    var geometryUrl = node.find("instance_geometry").attr("url");
-    if (geometryUrl) {
-      var geometry = doc.find(geometryUrl);
-      var accessor = geometry.find("technique_common accessor").attr("source");
-      var $positions = geometry.find(accessor);
-      var $polylist = geometry.find("polylist");
 
-      ret.geometry = getPolygons($positions, $polylist);
+    var scale = { x: 1, y: 1 };
+    var boundingBox = _(c.scene.children).chain().
+      filter(function(node){
+        var z = Math.round(node.position.z);
+        return (z == Z_GROUND || z == Z_WATER) && node.geometry;
+      }).
+      map(function (node) { return node.geometry.boundingBox; }).  
+      reduce(function (a, b) {
+        if (!a) return b;
+        if (!b) return a;
+        return {
+          min: {
+            x: Math.min(a.min.x, b.min.x),
+            y: Math.min(a.min.y, b.min.y)
+          },
+          max: {
+            x: Math.max(a.max.x, b.max.x),
+            y: Math.max(a.max.y, b.max.y)
+          }
+        };
+      }, null).
+      value();
+
+    function addCandle (node) {
+      map.objects.candles.push({ x: node.position.x, y: node.position.y });
     }
-    return ret;
-  }).toArray();
-  */
 
-  //var loader = new THREE.ColladaLoader();
-  //var obj = loader.parse(collada);
-  //console.log(obj);
+    function setWorldScale (node) {
+      scale = { x: node.scale.x, y: node.scale.y };
+    }
 
-  this.convert = function (callback) {
-    if (result)
-      callback(result);
-    else
-      cb = callback;
+    function setStart (node) {
+      map.start = { x: node.position.x, y: node.position.y };
+    }
+
+    function mapVerticesAndFaces (node) {
+      var g = node.geometry;
+      return {
+        vertices: _(g.vertices).map(function (v) {
+          return { x: v.position.x, y: v.position.y };
+        }),
+        faces: _(g.faces).map(function (f) {
+          var p = [];
+          if (f.a !== undefined)
+            p.push(f.a);
+          if (f.b !== undefined)
+            p.push(f.b);
+          if (f.c !== undefined)
+            p.push(f.c);
+          if (f.d !== undefined)
+            p.push(f.d);
+          return p;
+        })
+      }
+    }
+
+    function addGround (node) {
+      map.objects.grounds.push(mapVerticesAndFaces(node));
+    }
+
+    function addWater (node) {
+      map.objects.waters.push(mapVerticesAndFaces(node));
+    }
+
+    function addNoOxygen (node) {
+      map.objects.noOxygens.push(mapVerticesAndFaces(node));
+    }
+
+    _(c.scene.children).each(function(node){
+      switch (Math.round(node.position.z)) {
+        case Z_GROUND: return addGround(node);
+        case Z_CANDLE: return addCandle(node);
+        case Z_START:  return setStart(node);
+        case Z_WATER:  return addWater(node);
+        case Z_NO_OXYGEN: return addNoOxygen(node);
+        case Z_WORLD_SCALE: return setWorldScale(node);
+      }
+    });
+
+    map.width = (boundingBox.max.x-boundingBox.min.x)/scale.x;
+    map.height = (boundingBox.max.y-boundingBox.min.y)/scale.y;
+
+    function transformPosition (p) {
+      p.x = (p.x-boundingBox.min.x)/scale.x;
+      p.y = (p.y-boundingBox.min.y)/scale.y;
+    }
+
+    transformPosition(map.start);
+    _(map.objects.candles).each(transformPosition);
+    _(map.objects.grounds).each(function (g) {
+      _(g.vertices).each(transformPosition);
+    });
+    _(map.objects.waters).each(function (g) {
+      _(g.vertices).each(transformPosition);
+    });
+    _(map.objects.noOxygens).each(function (g) {
+      _(g.vertices).each(transformPosition);
+    });
+
+    return map;
   }
 }
 
-exports.Converter = Converter;
+window.Converter = Converter;
+
+}());
